63059a024a44e1dda6eddd12e877a2868c98043d
clipp-ing <62038266+clipp-ing@users.noreply.github.com>
2022-09-08 17:43:54 -0600

    Update query_monthly.sql

--- projects/flights/query_monthly.sql
+++ projects/flights/query_monthly.sql
@@ -1,13 +1,54 @@
-/*** Monthly Airline Delay Query ***/
-SELECT DATEPART(, month, Airlines.code, Airlines.description AS name, avg_dep_delay, avg_arr_delay
+USE flight_delays
+
+/*** Monthly Airline Delays ***/
+SELECT DATEPART(Year, date) AS year, 
+	DATEPART(Month, date) AS month, 
+	airline, 
+	Airlines.description AS name, 
+	COUNT(airline) as num_flights,
+	AVG(dep_delay) as avg_dep_delay, 
+	AVG(arr_delay) AS avg_arr_delay,
+	AVG(distance) as avg_distance,
+	STDEV(distance) as stdev_distance
 FROM Flights
 LEFT JOIN Airlines
 ON Flights.airline=Airlines.code
-ORDER BY year, month, Airlines.code, Airlines.description
+GROUP BY DATEPART(Year, date), DATEPART(Month, date), airline, Airlines.description
+ORDER BY DATEPART(Year, date), DATEPART(Month, date), airline
+GO
 
-/*** Monthly Airport Origin Query ***/
-SELECT year, month, Airports.code, Airports.name AS name, avg_dep_delay, avg_arr_delay
-FROM flights_monthly
+/*** Monthly Airport Origin ***/
+SELECT DATEPART(Year, date) as year, 
+	DATEPART(Month, date) as month, 
+	origin as origin_airport, 
+	Airports.city as origin_city, 
+	Airports.state as origin_state,
+	COUNT(origin) as num_flights,
+	AVG(dep_delay) as avg_dep_delay,
+	AVG(arr_delay) as avg_arr_delay,
+	AVG(crs_elapsed_time) as avg_crs_elapsed_time,
+	AVG(actual_elapsed_time) as avg_elapsed_time
+FROM Flights
 LEFT JOIN Airports
-ON flights_monthly.airline=Airlines.code
-ORDER BY year, month, Airlines.code, Airlines.description
+ON Flights.origin=Airports.code
+GROUP BY DATEPART(Year, date), DATEPART(Month, date), origin, Airports.city, Airports.state
+ORDER BY DATEPART(Year, date), DATEPART(Month, date), origin
+GO
+
+/*** Monthly Airport Destination ***/
+SELECT DATEPART(Year, date) as year, 
+	DATEPART(Month, date) as month, 
+	destination as destination_airport, 
+	Airports.city as destination_city, 
+	Airports.state as destination_state,
+	count(destination) as num_flights,
+	AVG(dep_delay) as avg_dep_delay,
+	AVG(arr_delay) as avg_arr_delay,
+	AVG(crs_elapsed_time) as avg_crs_elapsed_time,
+	AVG(actual_elapsed_time) as avg_elapsed_time
+FROM Flights
+LEFT JOIN Airports
+ON Flights.destination=Airports.code
+GROUP BY DATEPART(Year, date), DATEPART(Month, date), destination, destination, Airports.city, Airports.state
+ORDER BY DATEPART(Year, date), DATEPART(Month, date), destination
+GO
